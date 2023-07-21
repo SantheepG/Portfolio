663fa332ca1d6daff971e803c1dbd1172ccc964b
[Santheep Ganeswaran] <[gshantheep@gmail.com]>
2023-07-21 14:42:24 +0530

    nav changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Home from './components/Home/Home'
-import Skills from './components/Skills/Skills'
-import Contact from './components/Contact/Contact'
+import "./App.css";
+import Home from "./components/Home/Home";
+import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
 function App() {
   return (
     <>
-    <Home/>
-    <Skills/>
-    <Contact/>
+      <Home />
+      <Skills />
+      <Contact />
     </>
   );
 }
